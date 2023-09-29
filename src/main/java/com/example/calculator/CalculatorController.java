60c45e7f5211aef5e4ca13dc9ddd3ba72939128f
annamoskviceva <Annab.spb@mail.ru>
2023-09-29 23:40:55 +0300

    исправлено

--- src/main/java/com/example/calculator/CalculatorController.java
+++ src/main/java/com/example/calculator/CalculatorController.java
@@ -6,41 +6,48 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
+
 @RequestMapping("/calculator")
+
+
 public class CalculatorController {
 
+    private CalculatorService calculatorService = new CalculatorService();
+
+    @GetMapping
     public String greeting() {
-        CalculatorService calculatorService = new CalculatorService();
+
+    CalculatorService calculatorService = new CalculatorService();
+
         return calculatorService.greeting();
     }
 
-        public  String plus (@RequestParam int num1, @RequestParam int num2){
-            num1 = 5; // добавлено
-            num2 = 5;
-            CalculatorService calculatorService = new CalculatorService();
+
+    @GetMapping("/plus")
+        public  int plus (@RequestParam int num1, @RequestParam int num2){
+
+        CalculatorService calculatorService = new CalculatorService();
             return calculatorService.plus(num1,num2);
     }
+    @GetMapping("/minus")
+    public int minus( @RequestParam int num1,  @RequestParam int num2) {
 
-
-    public String minus(@RequestParam int num1, @RequestParam int num2) {
-        num1 = 5;
-        num2 = 5;
         CalculatorService calculatorService = new CalculatorService();
         return calculatorService.minus(num1,num2);
     }
 
+    @GetMapping("/multiply")
+    public int multiply(@RequestParam int num1, @RequestParam int num2) {
 
-    public String multiply(@RequestParam int num1, @RequestParam int num2) {
-        num1 = 5;
-        num2 = 5;
         CalculatorService calculatorService = new CalculatorService();
         return calculatorService.multiply(num1, num2);
     }
 
+    @GetMapping("/divide")
 
-    public String divide(@RequestParam int num1, @RequestParam int num2) {
-        num1 = 5;
-        num2 = 5;
+    public int divide( @RequestParam int num1, @RequestParam int num2) {
+
         CalculatorService calculatorService = new CalculatorService();
         return calculatorService.divide(num1,num2);
     }
--- src/main/java/com/example/calculator/CalculatorService.java
+++ src/main/java/com/example/calculator/CalculatorService.java
@@ -1,42 +1,39 @@
 package com.example.calculator;
 
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 import java.awt.geom.Arc2D;
 
 @Service
-@RequestMapping
+
 public class CalculatorService {
-    @GetMapping
     public String greeting() {
         return "Добро пожаловать в калькулятор!";
     }
 
-    public String plus(int num1, int num2) {
-        int result = num1 + num2;
-        return num1 + "+"+num2 + "=" + result ;
+    public int plus(int num1, int num2) {
+
+        return num1 + num2;
     }
 
 
 
-    public String minus(int num1, int num2) {
-        int result = num1 - num2;
-        return num1 + "-" + num2 + "=" + result;
+    public int minus(int num1, int num2) {
+
+        return num1 - num2;
     }
 
 
-    public String multiply(int num1, int num2) {
-        int result = num1 * num2;
-        return num1 + "*" + num2 + "=" + result;
+    public int multiply(int num1, int num2) {
+
+        return num1 *num2;
     }
 
 
-    public String divide(int num1, int num2) {
-        int result = num1 / num2;
-        return num1 + "/" + num2 + "=" + result;
+    public int divide(int num1, int num2) {
+
+        return  num1 / num2;
     }
 }
 
